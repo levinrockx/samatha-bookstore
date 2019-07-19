9c56ff477088ddafff9c257274c8681c72dec3c3
Levin E P <levinrockx@gmail.com>
2019-07-19 17:13:56 +0530

    Config default values update
    Config default values update

--- src/app/Config/config.service.ts
+++ src/app/Config/config.service.ts
@@ -5,25 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
 
-  // public API_ENDPOINT = 'http://192.168.43.212:5000';
-  public API_ENDPOINT = 'http://10.6.1.193:5000';
+  public API_ENDPOINT = '';
 
   public API_ENDPOINT_NAMES = {
-    blogall: '/blogall',
-    login: '/login',
-    blogadd: '/blogadd',
-    blogupdate: '/blogupdate',
-    blogdelete: '/blogdelete',
-    relatedblog: '/relatedblog',
-    blogpost: '/blogpost',
-    contactus: '/contactus',
   };
 
-  public VERSION_NUMBER = 'v0.1.6';
+  public VERSION_NUMBER = 'v0.0.0';
 
   public TOCKEN = {
-    keyname : 'tocken',
-    keyvalue : 'botzadmin',
+    keyname: '',
+    keyvalue: '',
   };
+
   constructor() { }
 }
