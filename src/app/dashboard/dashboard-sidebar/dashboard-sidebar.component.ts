b467e7b6e4a446e96c1ac5f223758eddebc0c9ff
Levin E P <levinrockx@gmail.com>
2019-07-19 20:55:31 +0530

    Angular font awesome and side bar responsive
    Angular font awesome and side bar responsive

--- src/app/dashboard/dashboard-sidebar/dashboard-sidebar.component.ts
+++ src/app/dashboard/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faHome, faUsersCog, faCogs, faPowerOff } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-dashboard-sidebar',
   templateUrl: './dashboard-sidebar.component.html',
   styleUrls: ['./dashboard-sidebar.component.scss']
 })
 export class DashboardSidebarComponent implements OnInit {
+  public faHome = faHome;
+  public faUsersCog = faUsersCog;
+  public faCogs = faCogs;
+  public faPowerOff = faPowerOff;
+  public selectedLink = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  navSelection(id) {
+    this.selectedLink = id;
   }
 
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,12 +4,13 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardlandingComponent } from './dashboardlanding/dashboardlanding.component';
 import { DashboardSidebarComponent } from './dashboard-sidebar/dashboard-sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [DashboardlandingComponent, DashboardSidebarComponent, DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FontAwesomeModule,
   ]
 })
 export class DashboardModule { }
