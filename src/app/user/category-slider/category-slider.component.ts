68f69d11502a2b334ef24967d1370019ab98f434
dev-ashwin-kumar <ashwinkarivellur74@gmail.com>
2019-07-21 22:37:30 +0530

    category icon
    category icon

--- src/app/user/category-slider/category-slider.component.ts
+++ src/app/user/category-slider/category-slider.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faPenNib, faHandHoldingHeart, faUserGraduate, faBookReader,faChild,faMusic } from '@fortawesome/free-solid-svg-icons';
+import { faPlayCircle, faIdBadge } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-category-slider',
   templateUrl: './category-slider.component.html',
   styleUrls: ['./category-slider.component.scss']
 })
 export class CategorySliderComponent implements OnInit {
-
+  public faPenNib = faPenNib;
+  public faPlayCircle = faPlayCircle;
+  public handHoldingHeart = faHandHoldingHeart;
+  public faUserGraduate = faUserGraduate;
+  public faBookReader = faBookReader;
+  public faIdBadge = faIdBadge;
+  public faChild = faChild;
+  public faMusic = faMusic;
   constructor() { }
 
   ngOnInit() {
