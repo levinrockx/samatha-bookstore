fc9156c82cc6f6bde7a8028a927a6caea31b8048
Vysakh kp <52004383+rublicon8682@users.noreply.github.com>
2019-07-23 01:03:10 +0530

    component creation and gmaps
    component creation and gmaps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NguiMapModule} from '@ngui/map';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=MY_GOOGLE_API_KEY'})
   ],
   providers: [],
   bootstrap: [AppComponent]
