b1deda5fa09b4176e150de15372b387ac7bd1784
dev-ashwin-kumar <ashwinkarivellur74@gmail.com>
2019-07-23 01:16:01 +0530

    book page gesign and imlimentation
    book page gesign and imlimentation

--- src/app/user/book-page/book-page.component.ts
+++ src/app/user/book-page/book-page.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-page.component.scss']
 })
 export class BookPageComponent implements OnInit {
-
+  public imagePath = 'assets/img/book.png';
+  public title = 'The History of Alappuzha: Travels on the roads of Red Clay';
+  public price = '₹100.00';
+  public category = 'Memories';
+  public author = 'Author';
+  public edition = 'First';
+  public language = ' Malayalam';
+  public content = 'The history of Alappuzha named Chemmanninle Nattuvazhikal edited by G. Sasidharan Pillai is a galleria of the beauty and heritage of one of the most beautiful district of Kerala. Alappuzha is enriched by the heritage of coir, fishes and boat races. The far reaching Vembanad lake is a feast to the eyes. The mouth watering food of Alappuzha attracts many to this place. Undoubtedly it can be  assured that this book on the Venice of the East , as Alappuzha is called, a guide to the travelers.  More over this is a historical footage on how the place is marked in the political and cultural renaissance of Kerala';
+  public Publisher = 'Samatha';
   constructor() { }
 
   ngOnInit() {
