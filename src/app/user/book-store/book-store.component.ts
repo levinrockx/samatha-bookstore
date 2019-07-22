23e4856293c95352ce4a584ab49591d8362da4b1
Levin E P <levinrockx@gmail.com>
2019-07-23 02:30:43 +0530

    Sprint two fixes
    Sprint two fixes

--- src/app/user/book-store/book-store.component.ts
+++ src/app/user/book-store/book-store.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ConfigService } from '../../Config/config.service';
+import { HttplayerService } from '../../HttpService/httplayer.service';
 @Component({
   selector: 'app-book-store',
   templateUrl: './book-store.component.html',
   styleUrls: ['./book-store.component.scss']
 })
 export class BookStoreComponent implements OnInit {
-
-  constructor() { }
+  public categoryList = [];
+  constructor(
+    private config: ConfigService,
+    private httpLayer: HttplayerService
+  ) { }
 
   ngOnInit() {
+    this.getBookAll();
+  }
+
+  getBookAll() {
+    const url = `${this.config.API_ENDPOINT}${this.config.API_ENDPOINT_NAMES.bookall}`;
+    // this.httpLayer.get(url).subscribe((response) => {
+    this.httpLayer.get('assets/json/bookall.json').subscribe((response) => {
+      if (response['status'] == 'success') {
+        this.categoryList = response['data']['categories'];
+      }
+    })
   }
 
 }
