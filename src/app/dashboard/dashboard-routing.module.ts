aec486fa0d76d50ba5e568b9a08d9b9d3b7dfdc7
Levin E P <levinrockx@gmail.com>
2019-07-27 17:50:28 +0530

    Login page and version page fix
    Login page and version page fix

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ChildrenOutletContexts } from '@angular/router';
 import { DashboardlandingComponent } from './dashboardlanding/dashboardlanding.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { VersionComponent } from './version/version.component';
 const routes: Routes = [
   {
     path: '',
@@ -16,6 +16,10 @@ const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
+      },
+      {
+        path: 'settings',
+        component: VersionComponent,
       }
     ],
   }
